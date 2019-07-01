f41f09ba2a3c2da1f1e46730e341c16119c4517a
jethros <jethro.sun7@gmail.com>
2019-07-01 17:17:33 -0400

    Add my setup

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# large traces
+#
+sudo ./build/MoonGen examples/pcap/replay-pcap.lua 0 top500-trace1.pcap
+#sudo ./build/MoonGen examples/pcap/replay-pcap.lua -l 0 top500-trace1.pcap
+
+
+# small traces
+#
+#sudo ./build/MoonGen examples/pcap/replay-pcap.lua  0 data/badssl-cap.pcap
